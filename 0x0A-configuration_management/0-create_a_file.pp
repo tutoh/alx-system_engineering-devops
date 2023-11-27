7405d0273e06adce17a9acefb76b77b105ca584d
tutoh <gicherudianagee@gmail.com>
2023-11-27 11:31:18 -0800

    new

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# site.pp or yourmodule/manifests/init.pp
+
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
